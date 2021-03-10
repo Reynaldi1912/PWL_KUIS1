1b56a6b0f36a37e515acbe364b39b44953300e41
= <nexrey19@gmail.com>
2021-03-10 14:30:44 +0700

    make view barang and show data from database

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\barang;
+
 
 class BarangController extends Controller
 {
-    //
+    public function index(){
+        $data_barang = Barang::all();
+        return view('barang', compact('data_barang'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/barang', [BarangController::class, 'index']);
+
 
